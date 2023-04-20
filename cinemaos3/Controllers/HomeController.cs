5bb8656f7edde396416cd538f08bf24e90d217e4
syb scholing <110843@st-maartenscollege.nl>
2023-04-20 11:57:29 +0200

    contact pagina gemaakt

--- cinemaos3/Controllers/HomeController.cs
+++ cinemaos3/Controllers/HomeController.cs
@@ -37,6 +37,10 @@ namespace cinemaos3.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
