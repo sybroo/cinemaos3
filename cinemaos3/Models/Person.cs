4be15800bc6a465278c0338bf6b2f9181a06576f
syb scholing <110843@st-maartenscollege.nl>
2023-06-05 12:02:01 +0200

    conatct pagina required gemaakt

--- cinemaos3/Models/Person.cs
+++ cinemaos3/Models/Person.cs
@@ -4,12 +4,15 @@ namespace cinemaos3.Models
 {
     public class Person
     {
-        [Required]
+        [Required(ErrorMessage = "Voornaam is een verplicht veld")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Achternaam is een verplicht veld")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Geldig e-mailadress invullen")]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
+        [Required(ErrorMessage = "Bericht is een verplicht veld")]
         public string Message { get; set; }
 
     }
